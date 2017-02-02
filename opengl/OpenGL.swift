91f2ec09876cbc4fefd5b14c77a9a8b56c7008e6
Janusz <janusz@janusz-ma.local>
2017-02-02 14:30:59 +0000

    Adding vertex and fragment shaders

--- opengl/OpenGL.swift
+++ opengl/OpenGL.swift
@@ -13,6 +13,8 @@ import GLKit
 class OpenGL:UIView {
 
   var colorRenderBuffer = GLuint()
+  var colorSlot = GLuint()
+  var positionSlot = GLuint()
   
   override class var layerClass:AnyClass {
     get {
@@ -38,6 +40,8 @@ class OpenGL:UIView {
     EAGLContext.setCurrent(context)
     
     setupBuffers()
+    compileShaders()
+    
     render()
   }
   
@@ -61,5 +65,76 @@ class OpenGL:UIView {
     glGenFramebuffers(1, &frameBuffer)
     glBindFramebuffer(GLenum(GL_FRAMEBUFFER), frameBuffer)
     glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_RENDERBUFFER), colorRenderBuffer)
+  }
+}
+
+extension OpenGL {
+  
+  fileprivate func compileShaders() {
+    
+    guard let vertex = compileShader(shader: "SimpleVertex", with: GLenum(GL_VERTEX_SHADER)), let fragment = compileShader(shader: "SimpleFragment", with: GLenum(GL_FRAGMENT_SHADER)) else {
+      print("Error with shaders")
+      return
+    }
+    
+    let programHandle = glCreateProgram()
+    glAttachShader(programHandle, vertex)
+    glAttachShader(programHandle, fragment)
+    glLinkProgram(programHandle)
+    
+    var success = GLint()
+    glGetProgramiv(programHandle, GLenum(GL_LINK_STATUS), &success)
+    
+    if success == GL_FALSE {
+      print("Something when wrong")
+    }
+    
+    glUseProgram(programHandle)
+    
+    positionSlot = GLuint(glGetAttribLocation(programHandle, "Position"))
+    colorSlot = GLuint(glGetAttribLocation(programHandle, "SourceColor"))
+    glEnableVertexAttribArray(positionSlot)
+    glEnableVertexAttribArray(colorSlot)
+  }
+  
+  fileprivate func compileShader(shader name:String,with type:GLenum)->GLuint? {
+  
+    guard let shaderPath = Bundle.main.path(forResource: name, ofType: "glsl", inDirectory:"shaders/",forLocalization:nil) else {
+      print("Error! \(name) not found")
+      return nil
+    }
+    
+    do {
+      let shaderString = try String(contentsOfFile: shaderPath, encoding: .utf8)
+      var shaderStringUTF8 = UnsafePointer<GLchar>(shaderString.cString(using: .utf8))
+      
+      let shaderHandle = glCreateShader(type)
+      var shaderStringLength = GLint(shaderString.characters.count)
+      
+      glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength)
+      
+      glCompileShader(shaderHandle)
+      
+      var success = GLint()
+      glGetShaderiv(shaderHandle, GLenum(GL_COMPILE_STATUS), &success)
+      
+      if success == GL_FALSE {
+        let infoLog = UnsafeMutablePointer<GLchar>.allocate(capacity: 256)
+        var infoLogLength = GLsizei()
+      
+        glGetShaderInfoLog(shaderHandle, GLsizei(MemoryLayout<GLchar>.size * 256), &infoLogLength, infoLog)
+        print("compileShader() : glCompileShader failed:",String(cString: infoLog))
+        
+        infoLog.deallocate(capacity: 256)
+        exit(1)
+      }
+      
+      return shaderHandle
+      
+    } catch {
+      print("Error! \(error.localizedDescription)")
+    }
+    
+    return nil
   }
 }
